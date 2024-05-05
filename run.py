31bf778093366721f44f9b7e339df04b310bccbb
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2024-05-05 16:55:25 +0300

    feat: add run script for start test

---
+++ run.py
@@ -0,0 +1,12 @@
+import subprocess
+
+
+def scale_selenium_nodes(node_count):
+    command = f"docker-compose up --build --scale chrome={node_count}"
+    subprocess.run(command, shell=True, check=True)
+
+
+if __name__ == "__main__":
+    node_count = 1
+    scale_selenium_nodes(node_count)
+    print(f"Chrome nodes are scaled to {node_count}")
